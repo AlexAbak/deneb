b653ab1bed2e3899487a13cdc450126c7852e636
AlexAbak <drum@pisem.net>
2013-05-13 01:24:32 +0400

    Трансформатор использует схему destination
    Signed-off-by: AlexAbak <drum@pisem.net>

--- transformation/src/org/deneblingvo/transformator/Transformator.java
+++ transformation/src/org/deneblingvo/transformator/Transformator.java
@@ -3,19 +3,19 @@ package org.deneblingvo.transformator;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Vector;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.xpath.XPathExpressionException;
 
+import net.sf.saxon.s9api.DOMDestination;
 import net.sf.saxon.s9api.Processor;
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.Serializer;
-import net.sf.saxon.s9api.XdmDestination;
 import net.sf.saxon.s9api.XdmNode;
 import net.sf.saxon.s9api.XsltCompiler;
 import net.sf.saxon.s9api.XsltExecutable;
@@ -23,24 +23,120 @@ import net.sf.saxon.s9api.XsltTransformer;
 
 import org.deneblingvo.serialization.xml.Reader;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Transformator {
 
 	/**
+	 * @param source
+	 * @return
 	 * @throws ParserConfigurationException 
 	 * @throws IOException 
 	 * @throws SAXException 
+	 */
+	private DOMSource createSource(Vector<Source> source) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+		Document document = documentBuilder.newDocument();
+		Node root = document.createElementNS("http://deneblingvo.org/xsd/transformator/list/1.0", "lst:list");
+		document.appendChild(root);
+		for (int i = 0; i < source.size(); i++) {
+			File tsFile = new File(source.get(i).href);
+			Document tsDocument = documentBuilder.parse(tsFile);
+			Node tsNode = document.adoptNode(tsDocument.getDocumentElement());
+			root.appendChild(tsNode);
+		}
+		return new DOMSource(document);
+	}
+
+	/**
+	 * @param destination
+	 * @return
+	 * @throws ParserConfigurationException 
+	 */
+	private Document createDestination() throws ParserConfigurationException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+		return documentBuilder.newDocument();
+	}
+
+	/**
+	 * @param destination
+	 * @param dest
+	 * @throws ParserConfigurationException 
+	 * @throws SaxonApiException 
+	 */
+	private void saveDestinations(Processor processor, Document destination, Destination dest) throws ParserConfigurationException, SaxonApiException {
+		Element element = destination.getDocumentElement();
+		NodeList nodes = element.getChildNodes();
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element cur = (Element)node;
+				String href = "";
+				if (cur.hasAttribute("href")) {
+					href = cur.getAttribute("href");
+				} else {
+					href = dest.href;
+				}
+				this.saveDestination(processor, cur, href);
+			}
+		}
+	}
+
+	/**
+	 * @param cur
+	 * @param href
+	 * @throws ParserConfigurationException 
+	 * @throws SaxonApiException 
+	 */
+	private void saveDestination(Processor processor, Element cur, String href) throws ParserConfigurationException, SaxonApiException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+		Document document = documentBuilder.newDocument();
+		Element sourceRoot = this.getSourceRoot(cur);
+		Node root = document.adoptNode(sourceRoot);
+		document.appendChild(root);
+
+		File file = new File(href);
+		Serializer serialization = processor.newSerializer(file);
+		net.sf.saxon.s9api.DocumentBuilder builder = processor.newDocumentBuilder();
+		XdmNode node = builder.build(new DOMSource(document));
+		serialization.serializeNode(node);
+	}
+
+	/**
+	 * @param element
+	 * @return
+	 */
+	private Element getSourceRoot(Element element) {
+		NodeList nodes = element.getChildNodes();
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				return (Element)node;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param transformationStream
+	 * @return
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws ParserConfigurationException 
 	 * @throws XPathExpressionException 
 	 * @throws IllegalAccessException 
 	 * @throws InstantiationException 
 	 * @throws SecurityException 
 	 * @throws NoSuchFieldException 
-	 * @throws TransformerException 
 	 * @throws SaxonApiException 
 	 */
-	public void transformate(InputStream transformationStream) throws ParserConfigurationException, SAXException, IOException, NoSuchFieldException, SecurityException, InstantiationException, IllegalAccessException, XPathExpressionException, TransformerException, SaxonApiException {
+	public void transformate(InputStream transformationStream) throws NoSuchFieldException, SecurityException, InstantiationException, IllegalAccessException, XPathExpressionException, ParserConfigurationException, SAXException, IOException, SaxonApiException {
 
 		Transformation transformation = new Transformation();
 		Reader reader = new Reader();
@@ -52,28 +148,12 @@ public class Transformator {
 		XsltExecutable xsltExecutable = xsltCompiler.compile(stylesheetSource);
 		XsltTransformer xsltTransformer = xsltExecutable.load();
 
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
-		Document document = documentBuilder.newDocument();
-		Node root = document.createElementNS("http://deneblingvo.org/xsd/transformator/list/1.0", "lst:list");
-		document.appendChild(root);
-		
-		for (int i = 0; i < transformation.source.size(); i++) {
-			File tsFile = new File(transformation.source.get(i).href);
-			Document tsDocument = documentBuilder.parse(tsFile);
-			Node tsNode = document.adoptNode(tsDocument.getDocumentElement());
-			root.appendChild(tsNode);
-		}
-		DOMSource source = new DOMSource(document);
-		xsltTransformer.setSource(source);
-
-		XdmDestination destination = new XdmDestination();
-		xsltTransformer.setDestination(destination);
+		xsltTransformer.setSource(this.createSource(transformation.source));
+		Document destination = this.createDestination();
+		xsltTransformer.setDestination(new DOMDestination(destination));
 		xsltTransformer.transform();
-		XdmNode xdmNode = destination.getXdmNode();
-		File file = new File(transformation.destination.href);
-		Serializer serializer = processor.newSerializer(file);
-		serializer.serializeNode(xdmNode);
+		
+		this.saveDestinations(processor, destination, transformation.destination);
 	}
 
 }
--- transformation/xml/test/stylesheet.xsl
+++ transformation/xml/test/stylesheet.xsl
@@ -3,6 +3,7 @@
 	version="2.0"
 	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
 	xmlns:lst="http://deneblingvo.org/xsd/transformator/list/1.0"
+	xmlns:dst="http://deneblingvo.org/xsd/transformator/destination/1.0"
 	xmlns:exm="http://deneblingvo.org/xsd/transformator/Example/1.0"
 	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 
@@ -11,18 +12,26 @@
 	<xsl:template
 		match="/">
 		<xsl:element
-			name="exm:database"
-			namespace="http://deneblingvo.org/xsd/transformator/Example/1.0">
-			<xsl:attribute 
-				name="xsi:schemaLocation"
-				namespace="http://www.w3.org/2001/XMLSchema-instance">http://deneblingvo.org/xsd/transformator/Example/1.0 Example.xsd</xsl:attribute>
-			<xsl:namespace
-				name="xsi">http://www.w3.org/2001/XMLSchema-instance</xsl:namespace>
-			<xsl:for-each
-				select="lst:list/exm:database">
-				<xsl:copy-of
-					select="exm:table" />
-			</xsl:for-each>
+			name="dst:destination"
+			namespace="http://deneblingvo.org/xsd/transformator/destination/1.0">
+			<xsl:element
+				name="dst:file"
+				namespace="http://deneblingvo.org/xsd/transformator/destination/1.0">
+				<xsl:element
+					name="exm:database"
+					namespace="http://deneblingvo.org/xsd/transformator/Example/1.0">
+					<xsl:attribute 
+						name="xsi:schemaLocation"
+						namespace="http://www.w3.org/2001/XMLSchema-instance">http://deneblingvo.org/xsd/transformator/Example/1.0 Example.xsd</xsl:attribute>
+					<xsl:namespace
+						name="xsi">http://www.w3.org/2001/XMLSchema-instance</xsl:namespace>
+					<xsl:for-each
+						select="lst:list/exm:database">
+						<xsl:copy-of
+							select="exm:table" />
+					</xsl:for-each>
+				</xsl:element>
+			</xsl:element>
 		</xsl:element>
 	</xsl:template>
 
